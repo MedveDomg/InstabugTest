04731f015468360eb0b5d7c23d4aab6f28f60f2d
vadim.zhdanov <zhdanov.vadim.alexandrovich@gmail.com>
2019-01-22 13:39:31 +0200

    setup instabug

---
+++ app/src/main/java/com/medvedomg/instabugtest/App.kt
@@ -0,0 +1,53 @@
+package com.medvedomg.instabugtest
+
+import android.app.Application
+import android.util.Log
+import com.instabug.library.Instabug
+import com.instabug.library.invocation.InstabugInvocationEvent
+import com.instabug.library.ui.onboarding.WelcomeMessage
+import com.instabug.survey.Surveys
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initInstabug()
+    }
+
+    private fun initInstabug() {
+        Instabug.Builder(this, "2892f7d4d81b8a821946e8ab96c0042b")
+            /**
+             * [Instabug.Builder.setInvocationEvents] in charges for triggering action for instabug dialog to save bugs,
+             * App doesn't need this feature
+             */
+            .setInvocationEvents(InstabugInvocationEvent.NONE)
+            .build()
+
+        /**
+         * SETUP DEBUG MODE
+         */
+        Instabug.setDebugEnabled(true)
+        /**
+         * SDK show welcome dialog by default,
+         * [Instabug.showWelcomeMessage] will disable auto showing welcome dialog
+         */
+        Instabug.showWelcomeMessage(WelcomeMessage.State.DISABLED)
+        /**
+         * [Surveys.setThresholdForReshowingSurveyAfterDismiss] will trigger showing survey,
+         * if user dismiss it,
+         * and doesn't respond for it
+         */
+        Surveys.setThresholdForReshowingSurveyAfterDismiss(3, 0)
+        /**
+         * SDK show survey after 10 seconds, when was initialised,
+         * [Surveys.setAutoShowingEnabled] will disable auto showing feature
+         */
+        Surveys.setAutoShowingEnabled(false)
+        Surveys.setOnShowCallback {
+            Log.d("OTP", "show survey callback")
+        }
+        Surveys.setOnDismissCallback {
+            Log.d("OTP", "dismiss survey callba ck")
+        }
+    }
+}
--- app/src/main/java/com/medvedomg/instabugtest/MainActivity.kt
+++ app/src/main/java/com/medvedomg/instabugtest/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.medvedomg.instabugtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.instabug.library.Instabug
+import com.instabug.survey.Surveys
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Instabug.setUserAttribute("user_id", Random().nextInt().toString())
+        Instabug.setUserAttribute("prize_earnings", Random().nextInt().toString())
+        Instabug.setUserAttribute("language", Locale.getDefault().language)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Handler().postDelayed({
+            Log.d("OTP","Surveys.getAvailableSurveys(): ${Surveys.getAvailableSurveys()}")
+            if (Surveys.getAvailableSurveys().isNotEmpty()) {
+                Surveys.showSurveyIfAvailable()
+            }
+        }, 5000)
     }
 }
